4e0c054ccceeebbc691b24c7fc9aeb898addc667
grv20 <windroid20@gmail.com>
2020-08-18 17:18:21 +0530

    18th aug update

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,11 +1,12 @@
 from . import views
-from django.urls import path
+from django.urls import path, re_path
 
 app_name = "blogs"
 
 urlpatterns = [
 path('', views.index, name='index'),
-path('blogs/(?P<blog_id>\d+)/', views.blog, name='blog'),
+re_path(r'^blogs/(?P<blog_id>\d+)/$', views.blog, name='blog'),
 path('new_blog/', views.new_blog, name='new_blog'),
-path('edit_blog/(?P<blog_id>\d+)/', views.edit_blog, name='edit_blog'),
+re_path(r'^edit_blog/(?P<blog_id>\d+)/$', views.edit_blog, name='edit_blog'),
 ]
+
